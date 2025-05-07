06bdbbcb5ea759f3cd20528ad19065a29d28647c
RUN-Projects <82665975+RUN-Projects@users.noreply.github.com>
2025-05-07 10:52:25 -0500

    launch browser automatically in windows

--- windows/buildResources/appLauncher.bat
+++ windows/buildResources/appLauncher.bat
@@ -1,2 +1,14 @@
 SET APP_RESOURCES_DIR=.\lib\
-.\bin\server.exe
+start .\bin\server.exe
+SET URL=http://localhost:8000
+if exist "C:\Program Files\Mozilla Firefox\firefox.exe" (
+	start "" "C:\Program Files\Mozilla Firefox\firefox.exe" %URL%
+)
+if exist "C:\Program Files (x86)\Mozilla Firefox\firefox.exe" (
+	start "" "C:\Program Files (x86)\Mozilla Firefox\firefox.exe" %URL%
+)
+if not exist "C:\Program Files\Mozilla Firefox\firefox.exe" (
+	if not exist "C:\Program Files (x86)\Mozilla Firefox\firefox.exe" (
+		start "" %URL%
+	)
+)
