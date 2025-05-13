8f61cbdfe511c193608251481626a1f09b6d0898
RUN-Projects <82665975+RUN-Projects@users.noreply.github.com>
2025-05-12 19:55:06 -0500

    if not exists syntax for windows

--- windows/scripts/build_and_run.bat
+++ windows/scripts/build_and_run.bat
@@ -1,9 +1,9 @@
-if [ ! -f ../../local_server/target/debug/local_server ]; then
+if not exist ../../local_server/target/debug/local_server (
     echo "Building local server"
     cd ../../local_server
     cargo build --release
     cd ../windows/scripts
-fi
+)
 
 echo "Assembling build environment"
 node ./build.js
